749384bd1f8b8c2f3958294df97e35e82dd1080f
spacewalker96 <hmiyedhassan@gmail.com>
2020-06-05 04:04:56 +0100

    validation

--- register.php
+++ register.php
@@ -32,7 +32,72 @@
     <script src="js/jquery.js"></script>
 	
 	<!-- Script -->
+	<script type="text/javascript">
+        $(document).ready(function () {
 
+            $("#register").click(function () {
+
+                fname = $("#fname").val();
+                lname = $("#lname").val();
+                email = $("#email").val();
+                password = $("#password").val();
+
+                $.ajax({
+                    type: "POST",
+                    url: "connect_valid.php",
+                    data: "fname=" + fname + "&lname=" + lname + "&email=" + email + "&password=" + password,
+                    success: function (html) {
+                        if (html == 'true') {
+
+                            $("#add_err2").html('<div class="alert alert-success"> \
+                                                 <strong>Account</strong> processed. \ \
+                                                 </div>');
+
+                            window.location.href = "index.php";
+
+                        } else if (html == 'false') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> already in system. \ \
+                                                 </div>');                    
+
+                        } else if (html == 'fname') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>First Name</strong> is required. \ \
+                                                 </div>');
+												 
+						} else if (html == 'lname') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Last Name</strong> is required. \ \
+                                                 </div>');
+
+                        } else if (html == 'eshort') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> is required. \ \
+                                                 </div>');
+
+                        } else if (html == 'eformat') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Email Address</strong> format is not valid. \ \
+                                                 </div>');
+												 
+						} else if (html == 'pshort') {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Password</strong> must be at least 4 characters . \ \
+                                                 </div>');
+
+                        } else {
+                            $("#add_err2").html('<div class="alert alert-danger"> \
+                                                 <strong>Error</strong> processing request. Please try again. \ \
+                                                 </div>');
+                        }
+                    },
+                    beforeSend: function () {
+                        $("#add_err2").html("loading...");
+                    }
+                });
+                return false;
+            });
+        });
     </script>
 
 </head>
@@ -55,7 +120,7 @@
                     </h2>
 					<div id="add_err2"></div>
                     <hr>       
-                    <form role="form">
+                    <form role="form" action = "register.php" methode = "post">
                         <div class="row">
                             <div class="form-group col-lg-4">
                                 <label>First Name</label>
@@ -70,12 +135,8 @@
                                 <input type="email" id="email" name="email" maxlength="25" class="form-control">
                             </div>
                             <div class="clearfix"></div>
-                            <div class="form-group col-md-12">
+                            <div class="form-group col-lg-12">
                                 <label>Password</label>
-                                <input type="password" id="password" name="password" maxlength="10" class="form-control">
-                            </div>
-                            <div class="form-group col-md-12">
-                                <label>Repeat Password</label>
                                 <input type="password" id="password" name="password" maxlength="10" class="form-control">
                             </div>
                             <div class="form-group col-lg-12">
@@ -94,7 +155,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <p>Copyright &copy; The Perfect Cup 2019</p>
+                    <p>Copyright &copy; The Perfect Cup 2016</p>
                 </div>
             </div>
         </div>
